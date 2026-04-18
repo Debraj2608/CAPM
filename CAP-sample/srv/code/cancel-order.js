02375b8e7ef845ce6c3401e900082eba981bab15
Debraj2608 <debraj.sarmah025@gmail.com>
2026-04-19 02:39:52 +0530

    Added test cases for cancel order

--- CAP-sample/srv/code/cancel-order.js
+++ CAP-sample/srv/code/cancel-order.js
@@ -1,6 +1,11 @@
+const { SELECT } = require("@sap/cds/lib/ql/cds-ql");
+
 const cancelOrder = async (request) => {
     const orderID = request.params[0].ID;
-
+    const orderStatus = await SELECT.one('status_code').from('my.bookshop.Orders').where({ID: orderID});
+    if(orderStatus.status_code!== 'PLACED'){
+        return request.error(500, 'You cannot cancel an order after the order is not in placed status.');
+    }
     const orderItems = await SELECT.from('my.bookshop.OrderItems').where({order_ID: orderID});
     const books_ID = orderItems.map(book => book.book_ID);
     const books = await SELECT.from('my.bookshop.Books').where({ID: {in: books_ID}}).forUpdate();
--- CAP-sample/tests/CatalogService.test.js
+++ CAP-sample/tests/CatalogService.test.js
@@ -1,5 +1,8 @@
 const cds = require('@sap/cds')
-const place_order_succeds = require('./code/test-place-order-succeds');
+const place_order_succeds = require('./code/test-place-order');
+const place_order_fails = require('./code/test-place-order');
+const cancel_order_succeds = require('./code/test-cancel-order');
+const cancel_order_fails = require('./code/test-cancel-order');
 
 const { GET, POST, PATCH, DELETE, expect, axios } = cds.test (__dirname+'/..', '--with-mocks')
 // cds.env.requires.auth = {
@@ -12,7 +15,15 @@ describe('Catalog service books and manage orders testing', () => {
   });
 
   it('place order fails', async () => {
-    await place_order_succeds.placeOrderFails(GET, POST, PATCH, DELETE, expect);
+    await place_order_fails.placeOrderFails(GET, POST, PATCH, DELETE, expect);
   });
+
+  it('cancel order succeds', async () => {
+    await cancel_order_succeds.cancelOrderSucceds(GET, POST, PATCH, DELETE, expect);
+  });
+
+  it('cancel order fails', async () => {
+    await cancel_order_succeds.cancelOrderFails(GET, POST, PATCH, DELETE, expect);
+  })
 
 })
