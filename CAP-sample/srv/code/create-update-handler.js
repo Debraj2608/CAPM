605c26bdff6d430952f12dd9744c9bba42fbcb39
Debraj2608 <debraj.sarmah025@gmail.com>
2026-04-21 01:09:38 +0530

    Added deployment config

--- CAP-sample/srv/code/create-update-handler.js
+++ CAP-sample/srv/code/create-update-handler.js
@@ -2,7 +2,12 @@ module.exports = async (results, request) => {
     if (results.status_code === null || results.status_code === undefined) {
         await UPDATE('my.bookshop.Orders').set({ status_code: 'INCART' }).where({ ID: results.ID });
         const orderIDLatest = await SELECT('orderNumber').from('my.bookshop.Orders').orderBy({ modifiedAt: 'desc' }).where({ orderNumber: { '!=': null } });
-        const orderNumber = parseInt(orderIDLatest[0].orderNumber.match(/\d+/)[0]) + 1;
+        let orderNumber = "";
+        if(orderIDLatest.length == 0){
+            orderNumber = "0001";
+        } else {
+            orderNumber = parseInt(orderIDLatest[0].orderNumber.match(/\d+/)[0]) + 1;
+        }
         await UPDATE('my.bookshop.Orders').set({ orderNumber: 'ORD-' + orderNumber }).where({ ID: results.ID });
     }
     const orderItems = await SELECT.from('my.bookshop.OrderItems').where({ order_ID: results.ID });
